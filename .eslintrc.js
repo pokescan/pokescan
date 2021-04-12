93d485c101a2aaee7d966c95b17590efeef4dd12
canserkanuren <canserkan.uren@gmail.com>
2021-04-12 21:26:10 +0200

    :wrench:

---
+++ .eslintrc.js
@@ -0,0 +1,92 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'jest'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:jest/recommended',
+    'plugin:jest/style'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    semi: [2, 'always', { omitLastInOneLineBlock: true }],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'jest/no-test-return-statement': 'error',
+    'jest/no-alias-methods': 'error',
+    'jest/no-jasmine-globals': 'error',
+    'jest/expect-expect': [
+      'error',
+      {
+        assertFunctionNames: [
+          'expect',
+          'assertContentIsEqualTo',
+          'expectStatutIsCorrectlyDisplayed',
+          'assertContentIsHidden',
+          'assertContentIsDisplayed'
+        ]
+      }
+    ],
+    'jest/no-test-prefixes': 'error',
+    '@typescript-eslint/no-parameter-properties': 'off'
+  },
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['tsconfig.json'],
+        createDefaultProgram: true
+      },
+      extends: [
+        'plugin:@angular-eslint/ng-cli-compat',
+        'plugin:@angular-eslint/ng-cli-compat--formatting-add-on',
+        'plugin:@angular-eslint/template/process-inline-templates'
+      ],
+      rules: {
+        '@angular-eslint/component-class-suffix': [
+          'error',
+          {
+            suffixes: ['Page', 'Component']
+          }
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case'
+          }
+        ],
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase'
+          }
+        ]
+      }
+    },
+    {
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {}
+    }
+  ]
+};
