caf87ed713a5996aafa1f0d934136df6f6bbf11e
Julien MIFSUD <julien.mifsud77@gmail.com>
2021-07-05 18:28:39 +0200

    [PK-3] : Add tab bar navigation (#13)
    * feat(tabs): add tabs navigation
    
    * feat(tab): add tab bar navigation

---
+++ src/app/pages/tabs/tabs.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { TabsPageRoutingModule } from './tabs-routing.module';
+import { TabsPage } from './tabs.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule,
+    TranslateModule
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
