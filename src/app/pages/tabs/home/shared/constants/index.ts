75608c302e51b81ecf585c220b64753bba1e4547
Julien MIFSUD <julien.mifsud77@gmail.com>
2021-07-05 19:00:27 +0200

    [PKS-7]: Homepage (#15)
    * feat(homepage): add home card
    
    * feat(homepage): change import file

---
+++ src/app/pages/tabs/home/shared/constants/index.ts
@@ -0,0 +1,10 @@
+import { IHomeCard } from '../interfaces';
+
+export const HOME_CARDS: IHomeCard[] = [
+  {
+    label: 'POKEDEX',
+    color: '#E9573F',
+    icon: 'paw',
+    url: 'pokedex'
+  }
+];
---
+++ src/app/pages/tabs/home/shared/interfaces/index.ts
@@ -0,0 +1,6 @@
+export interface IHomeCard {
+  label: string;
+  icon: string;
+  url: string;
+  color: string;
+}
